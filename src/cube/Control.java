f6c1098e294d798f6f1bf844f57123f5556e25ec
PenguinAlex <besedinalexey@gmail.com>
2019-01-21 18:05:46 +0300

    Optimized Control.java

--- src/cube/Control.java
+++ src/cube/Control.java
@@ -5,27 +5,23 @@ import java.awt.event.*;
 
 public class Control {
     public Control(Cube cube, Window window) {
+        window.changeView.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                cube.altView = !cube.altView;
+                window.repaint();
+            }
+        });
         window.headingSlider.addChangeListener(new ChangeListener() {
             @Override public void stateChanged(ChangeEvent e) {
-                double currentAngle = cube.getAngles()[1], sliderValue = -window.headingSlider.getValue(), toChange = 0;
-                if (currentAngle > sliderValue) toChange = -(currentAngle - sliderValue);
-                if (currentAngle < sliderValue) toChange = sliderValue - currentAngle;
-                cube.rotate(0, toChange, 0);
+                double currentAngle = cube.getAngles()[1], sliderValue = -window.headingSlider.getValue();
+                cube.rotate(0, sliderValue - currentAngle, 0);
                 window.repaint();
             }
         });
         window.pitchSlider.addChangeListener(new ChangeListener() {
             @Override public void stateChanged(ChangeEvent e) {
-                double currentAngle = cube.getAngles()[0], sliderValue = -window.pitchSlider.getValue(), toChange = 0;
-                if (currentAngle > sliderValue) toChange = -(currentAngle - sliderValue);
-                if (currentAngle < sliderValue) toChange = sliderValue - currentAngle;
-                cube.rotate(toChange, 0, 0);
-                window.repaint();
-            }
-        });
-        window.changeView.addActionListener(new ActionListener() {
-            @Override public void actionPerformed(ActionEvent e) {
-                cube.altView = !cube.altView;
+                double currentAngle = cube.getAngles()[0], sliderValue = -window.pitchSlider.getValue();
+                cube.rotate(sliderValue - currentAngle, 0, 0);
                 window.repaint();
             }
         });
